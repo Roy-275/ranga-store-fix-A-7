315be71a37f99612c452cb5083d51e7c3a93f401
Roy-275 <anikroy275@gmail.com>
2021-09-12 23:30:37 +0600

    image showing now

--- js/app.js
+++ js/app.js
@@ -8,9 +8,10 @@ loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
+  console.log(products);
+  const allProducts = products.foreach((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
+    const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
